7a559c63df6350e99312db09b0e0c70908fc7bdb
nakki44 <107858026+nakki44@users.noreply.github.com>
2022-11-26 23:46:26 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 const fs=require('fs');
 const http = require('http');
 
-http.createServer((request, response) => {
-  const { headers, method, url } = request;
-
-  let body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-
-    response.writeHead(200)
-    const responseBody = { headers, method, url, body };
-    
-    
-    response.end(console.log(responseBody));
+http.createServer(function(req, res) {
+  // 서버생성시 같은 디렉토리에 있는 .html 파일을 읽어옴
+  fs.readFile('index.html', function(err, data) {
+      if(err) {
+          // 만약 file read 에 실패했을 경우
+          console.log('file read error');
+      }
+      else {
+          // file read 에 성공하면 data 에 .html 파일의 내용이 전달됨
+          res.writeHead(200, { 'Content-Type' : 'text/html'});
+          res.end(data);
+      }
   });
-}).listen(3000);
+}).listen(3000, function() {
+  console.log('server running at http://localhost:3000');
+});
